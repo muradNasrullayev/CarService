b806912e15bc1dade82e2fead3e235661fe3e9ce
muradNasrullayev <muradnesrullayev19@gmail.com>
2024-08-28 14:39:35 +0400

    Errors complate

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,3 @@
-@extends('front.main.app')
-@section('content')
 
     <div class="container-xxl py-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="container text-center">
@@ -9,9 +7,8 @@
                     <h1 class="display-1">404</h1>
                     <h1 class="mb-4">Axtardığınız səhifə tapılmadı</h1>
                     <p class="mb-4">Ana səhifəyə geri dönüş edə və ya menyudan hər hansı səhifəyə keçid edə bilersiniz</p>
-                    <a class="btn btn-primary py-3 px-5" href>Go Back To Home</a>
+                    <a class="btn btn-primary py-3 px-5" href="{{route('web.index')}}">Go Back To Home</a>
                 </div>
             </div>
         </div>
     </div>
-@endsection
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,5 +1,4 @@
-@extends('front.main.app')
-@section('content')
+
 
     <div class="container-xxl py-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="container text-center">
@@ -12,4 +11,4 @@
             </div>
         </div>
     </div>
-@endsection
+
